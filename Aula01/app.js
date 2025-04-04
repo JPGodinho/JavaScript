5efdd80768e85ad76b5cec6104d774738f2124f2
Joao Pedro Godinho Passiani <rm561602@fiap.com.br>
2025-04-03 22:42:42 -0300

    aula01

---
+++ Aula01/app.js
@@ -0,0 +1,15 @@
+alert("Olá mundo!")
+
+let nome = prompt("Qual o seu nome? ")
+let idade = prompt("Qual a sua idade?")
+let profissao = prompt("Qual a sua profissão")
+alert(`Olá ${nome}, tudo bem? Muito bacana que sua idade seja ${idade} e que você trabalhe com ${profissao}`)
+
+let resultado = confirm("Você deseja seguir?")
+
+let numero1 = Number(prompt("Digite um numero: "))
+let numero2 = Number(prompt("Digite um segundo: "))
+
+let resultado = numero1 + numero2
+
+console.log(String(resultado))
