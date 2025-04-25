4f3a76a9cff532ce1c9f21cf591e60c7635cdb5d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-24 22:44:08 -0300

    auala 03

---
+++ Aula03/app.js
@@ -0,0 +1,68 @@
+//Teoria
+// function pegaNome(){
+//     let nome = "joao"
+//     return nome
+// }
+
+// function exibirNome(nomeUsuario, sobrenome){
+//     nomeUsuario
+//     let resultado = pegaNome()
+//     console.log(resultado)
+// }
+
+// exibirNome("pedro", "Godinho")
+
+
+
+
+// function somaDoisNumeros(primeiroNumero, segundoNumero){
+//     return primeiroNumero + segundoNumero
+// }
+
+// console.log(somaDoisNumeros(5, 12))
+
+
+// const somaNumeros = (numero1, numero2) => numero1 + numero2
+
+let numero = 5.354
+let nome = "  Joao  "
+console.log(nome.length)
+
+if(nome.length < 3){
+    alert("isso nao é um nome")
+}
+
+console.log(nome.toUpperCase())
+console.log(nome.toLowerCase())
+console.log(nome.includes("c"))
+console.log(nome.includes("J"))
+console.log(nome.trim())
+console.log(numero.toFixed(2))
+
+
+// Exercícios
+
+// function receberNome(){
+//     let nome = prompt("Digite seu nome: ")
+//     return nome
+// }
+
+// function receberIdade(){
+//     let idade = prompt("Digite sua idade: ")
+//     return idade
+// }
+
+// function mostrarNomeIdade(nome, idade){
+//     alert(`Seu nome é: ${nome || "Fulano"} e a sua idade é: ${idade}`)
+// }
+
+// function rodarPrograma(){
+//     let nome = receberNome()
+//     let idade = receberIdade()
+
+//     mostrarNomeIdade(nome, idade)
+// }
+
+// rodarPrograma()
+
+
